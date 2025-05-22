914964305f6b63b6c706568620eaa77766893f2f
Fred Menge <mengeprofissional@gmail.com>
2025-05-22 09:13:11 -0300

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
+import prettier from 'eslint-plugin-prettier'
+import configPrettier from 'eslint-config-prettier'
+
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    languageOptions: { globals: globals.browser },
+    rules: {
+      // Prettier como regra de erro
+      'prettier/prettier': 'error'
+    }
+  },
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    // Configuração do Prettier para desativar regras conflitantes
+    plugins: { prettier },
+    rules: configPrettier.rules,
+    'react/react-in-jsx-scope': 'off'
+  }
+]
